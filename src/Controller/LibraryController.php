5bb5dc2a55348d83a98b7329f4eb12c46dd7db41
Pedro Tavares <pedro.tavares@apside-groupe.com>
2021-04-14 17:26:47 +0100

    changes in the dockerfile and folder structure of the project

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    /**
+     * @Route("/library", name="library")
+     */
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+}
